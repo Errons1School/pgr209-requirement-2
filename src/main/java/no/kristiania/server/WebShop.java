6131af08c388b2e65413a6d167556a799b2d0e3c
bjarte <22171847+freebattie@users.noreply.github.com>
2022-10-09 15:07:07 +0200

    🔴 getStatusCode200Test * removed test for testing github action

--- src/main/java/no/kristiania/server/WebShop.java
+++ src/main/java/no/kristiania/server/WebShop.java
@@ -1,9 +1,32 @@
 package no.kristiania.server;
 
-public class WebShop {
-    public static void main(String[] args) {
-        System.out.println("Hello World");
+import org.eclipse.jetty.server.Server;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class WebShop {
+    private final Server server;
+
+    public WebShop(int port) {
+        this.server = new Server(port);
+
+    }
+    public void start() throws Exception {
+
+        server.start();
+    }
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello World");
+        var serv = new WebShop(8080);
+        serv.start();
+        System.out.println("server started");
+
+
+    }
+
+    public URL getURL() throws MalformedURLException {
+        return server.getURI().toURL();
 
     }
 }
--- src/test/java/no/kristiania/server/WebShopTest.java
+++ src/test/java/no/kristiania/server/WebShopTest.java
@@ -2,13 +2,29 @@ package no.kristiania.server;
 
 import org.junit.jupiter.api.Test;
 
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 class WebShopTest {
+    private WebShop server;
 
-    @Test
-    public void gitHubActionTest(){
-        assertTrue(true);
+    private HttpURLConnection openConnection(String spec) throws IOException {
+        return (HttpURLConnection) new URL(server.getURL(), spec).openConnection();
     }
 
+
+    @Test
+    public void getStatusCode200Test() throws Exception {
+
+        this.server = new WebShop(0);
+        server.start();
+        var connection = openConnection("/");
+        assertThat(connection.getResponseCode()).as("check for 200").isEqualTo(200);
+
+    }
 }
