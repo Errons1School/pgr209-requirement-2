b27b18afadb3a4fed7e9eb71ce2afe35c7601be7
bjarte <22171847+freebattie@users.noreply.github.com>
2022-10-09 14:32:04 +0200

    🔴 githubactionTest -added dependencies to pom.xml

---
+++ src/test/java/no/kristiania/server/WebShopTest.java
@@ -0,0 +1,15 @@
+package no.kristiania.server;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class WebShopTest {
+
+    @Test
+    public void gitHubActionTest(){
+
+        assertTrue(false);
+    }
+
+}
